cd4091481021659c70551caa47e8fc9dc61854de
e-conte <160084751+e-conte@users.noreply.github.com>
2024-04-09 00:27:55 -0300

    computerSelection function Added.
    Also new Global Variable is Added.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /* variables*/
 let playerName = "";
 let gameCounter;
-
+/*
 function welcomePlayer() {
 
     while (playerName === "") {
@@ -13,3 +13,28 @@ function welcomePlayer() {
 }
 
 welcomePlayer();
+*/
+let computerTry;
+
+function computerSelection() {
+    let randomNumber = Number((Math.random() * 10).toFixed(3));
+    switch (true) {
+        case randomNumber <= 3.33:
+            {
+                computerTry = "ROCK";
+                break;
+            }
+        case randomNumber <= 6.66:
+            {
+                computerTry = "PAPER";
+                break;
+            }
+        case randomNumber <= 10:
+            {
+                computerTry = "SCISSORS";
+                break;
+            }
+    }
+return computerTry;    
+}
+computerSelection();
